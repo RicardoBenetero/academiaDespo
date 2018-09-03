2a88129a513ac9e0a6cbb385142d9c58b31dc355
aluno <aluno@10.32.16.106>
2018-09-03 10:58:10 -0300

    alteração no método diagnótico, criada variavel valorImc

--- ClaculaIMC/src/br/cousera/Paciente.java
+++ ClaculaIMC/src/br/cousera/Paciente.java
@@ -10,35 +10,30 @@ public class Paciente {
 		this.altura = altura;
 
 	}
-	
-	public double calcularIMC(){
-		return peso/(altura * altura);
+
+	public double calcularIMC() {
+		return peso / (altura * altura);
 	}
-	
-public String diagnostico(){
-		
-		if(calcularIMC() < 16){
+
+	public String diagnostico() {
+
+		double valorImc = calcularIMC();
+
+		if (valorImc < 16) {
 			return "Baixo peso muito grave";
-		}
-		else if(calcularIMC() >= 16 && calcularIMC() <= 16.99 ){
+		} else if (valorImc >= 16 && valorImc <= 16.99) {
 			return "Baixo peso grave";
-		}
-		else if(calcularIMC() >= 17 && calcularIMC() <= 18.49 ){
+		} else if (valorImc >= 17 && valorImc <= 18.49) {
 			return "Baixo peso ";
-		}
-		else if(calcularIMC() >= 18.50 && calcularIMC() <= 24.99 ){
+		} else if (valorImc >= 18.50 && valorImc <= 24.99) {
 			return "Peso normal ";
-		}
-		else if(calcularIMC() >= 25 && calcularIMC() <= 29.99 ){
+		} else if (valorImc >= 25 && valorImc <= 29.99) {
 			return "Sobrepeso";
-		}
-		else if(calcularIMC() >= 30 &&  calcularIMC() <= 34.99 ){
+		} else if (valorImc >= 30 && valorImc <= 34.99) {
 			return "Obesidade grau I ";
-		}
-		else if(calcularIMC() >= 35 && calcularIMC() <= 39.99 ){
+		} else if (valorImc >= 35 && valorImc <= 39.99) {
 			return "Obesidade grau II ";
-		}
-		else{
+		} else {
 			return "Obesidade grau III (obesidade mórbida)";
 		}
 	}
