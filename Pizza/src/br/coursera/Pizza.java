0d1e8c3acdc765ab811c49091016bc61dc2008e2
Ricardo <ricardo.decarvalho@serpro.gov.br>
2018-09-05 11:10:15 -0300

    ajuste no atributo preço da classe Pizza para preco

--- Pizza/src/br/coursera/Pizza.java
+++ Pizza/src/br/coursera/Pizza.java
@@ -3,7 +3,7 @@ package br.coursera;
 public class Pizza {
 	
 	String sabor;
-	double preço = 20.0;
+	double preco = 20.0;
 	
 	public Pizza(String sabor) {
 		
@@ -15,13 +15,13 @@ public class Pizza {
 	public double calculaPreco(){
 		
 		if(sabor == "C") {
-			preço = 25.0;
+			preco = 25.0;
 		}else if (sabor == "M") {
-			preço = 22.0;
+			preco = 22.0;
 		}else if (sabor == "P")
-			preço = 30.0;
+			preco = 30.0;
 		
-		return preço;
+		return preco;
 		
 		
 		
