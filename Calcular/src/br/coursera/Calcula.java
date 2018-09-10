3ca5513ba2ec0ae24720575ca352b5f3fafeb3c2
aluno <aluno@10.32.16.106>
2018-09-10 10:57:51 -0300

    projeto Calcular

---
+++ Calcular/src/br/coursera/Calcula.java
@@ -0,0 +1,39 @@
+package br.coursera;
+
+public class Calcula {
+	
+	
+	
+	
+	public static int retornaMaiorNumero(int numero1, int numero2) {
+		return 0;
+		
+		
+	}
+	
+	public static int retornaTemperaturaConvertida(int temperaturaEmGrausCelsius) {
+		return 0;
+		
+		
+	}
+	
+	public static int retornaNumeroAleatorio(int numero1, int numero2) {
+		return 0;
+		
+		
+	}
+	
+	public static boolean validaCPF(int cpf) {
+		return true;
+		
+		
+	}
+	
+	public static String formataCPF(int cpf) {
+		return null;
+		
+		
+		
+	}
+
+}
