27953e212546f2624c164d47910bb9bc8ce4c64c
Ricardo <ricardo.decarvalho@serpro.gov.br>
2018-09-11 13:49:07 -0300

    inclusao de metodo para formatar cpf

--- Calcular/src/br/coursera/Calcula.java
+++ Calcular/src/br/coursera/Calcula.java
@@ -1,39 +1,35 @@
 package br.coursera;
 
 public class Calcula {
-	
-	
-	
-	
+
 	public static int retornaMaiorNumero(int numero1, int numero2) {
 		return 0;
-		
-		
+
 	}
-	
+
 	public static int retornaTemperaturaConvertida(int temperaturaEmGrausCelsius) {
 		return 0;
-		
-		
+
 	}
-	
+
 	public static int retornaNumeroAleatorio(int numero1, int numero2) {
 		return 0;
-		
-		
+
 	}
-	
-	public static boolean validaCPF(int cpf) {
+
+	public static boolean validaCPF(String cpf) {
+
 		return true;
-		
-		
+
 	}
-	
-	public static String formataCPF(int cpf) {
-		return null;
+
+	public static void formataCPF(String cpf) {
 		
-		
-		
+		String cpfFormatado = (cpf.substring(0, 3) + "." + cpf.substring(3, 6)
+				+ "." + cpf.substring(6, 9) + "-" + cpf.substring(9, 11));
+
+		System.out.println(cpfFormatado);
+
 	}
 
 }
---
+++ Calcular/src/br/coursera/Principal.java
@@ -0,0 +1,11 @@
+package br.coursera;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Calcula calcula = new Calcula();
+
+		calcula.formataCPF("23569879674");
+	}
+}
