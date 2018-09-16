b5d73aec0c43c07b505e85283ba51cbe2587c941
Ricardo <Ricardo@192.168.0.17>
2018-09-16 00:28:08 -0300

    Exercicio metodos estaticos - implementacao de testes unitarios

--- Calcular/src/br/coursera/Calcula.java
+++ Calcular/src/br/coursera/Calcula.java
@@ -7,14 +7,14 @@ public class Calcula {
 	
 	
 	
-	public static void retornaMaiorNumero(int numero1, int numero2) {
+	public static int retornaMaiorNumero(int numero1, int numero2) {
 		
 		if (numero1 > numero2) {
 			
-			System.out.println("maior: " + numero1 );
+			return numero1 ;
 		}else {
 			
-			System.out.println("maior: " + numero2 );
+			return numero2 ;
 
 			
 			
@@ -23,35 +23,35 @@ public class Calcula {
 		
 	}
 	
-	public static void retornaTemperaturaConvertidaDeCelsiusParafahrenheit(int temperaturaEmGrausCelsius) {
+	public static double retornaTemperaturaConvertidaDeCelsiusParafahrenheit(int temperaturaEmGrausCelsius) {
 		
 		double fahrenheit = (9 * temperaturaEmGrausCelsius + 160 )/ 5;
 		
 		
-		System.out.println("Temperatura em fahrenheit eh: " + fahrenheit);
+		return  fahrenheit;
 		
 	}
 	
-	public static void retornaNumeroAleatorio(int numero1, int numero2) {
+	public static int retornaNumeroAleatorio(int numero1, int numero2) {
 		
 		
 		
 		Random aleatorio = new Random();
 		int valor = aleatorio.nextInt((numero2 - numero1) + 1) + numero1;;
-		System.out.println("Número gerado: " + valor);
+		return valor;
 		
 		
 	}
 	
 	
 	
-public static void formataCPF(String cpf) {
+public static String formataCPF(String cpf) {
         
         
         String cpfFormatado = (cpf.substring(0, 3) + "." + cpf.substring(3, 6)
                 + "." + cpf.substring(6, 9) + "-" + cpf.substring(9, 11));
 
-        System.out.println("CPF formatado - " + cpfFormatado);
+        return cpfFormatado;
 
     }
 
---
+++ Calcular/src/br/coursera/test/TestCalcula.java
@@ -0,0 +1,48 @@
+package br.coursera.test;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import br.coursera.Calcula;
+
+public class TestCalcula {
+
+	@Test
+	public void retornaMaiorNumero() {
+		
+		assertEquals(963, Calcula.retornaMaiorNumero(569, 963));
+		
+		
+	}
+	
+	@Test
+	public void retornaTemperaturaConvertidaDeCelsiusParafahrenheit() {
+		
+		assertEquals(59.0, Calcula.retornaTemperaturaConvertidaDeCelsiusParafahrenheit(15));
+		
+		
+	}
+
+	@Test
+	public void formataCPF() {
+		
+		assertEquals("235.698.796-74",Calcula.formataCPF("23569879674"));
+
+		
+		
+	}
+	
+	@Test
+	public void retornaNumeroAleatorio() {
+		
+		int numeroAleatorio = Calcula.retornaNumeroAleatorio(1, 4);
+		assertTrue( numeroAleatorio >= 1 && numeroAleatorio <= 4);
+
+		
+		
+	}
+
+
+}
