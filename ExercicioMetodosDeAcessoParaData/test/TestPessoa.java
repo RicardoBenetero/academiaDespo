b23d08e0b403892ef759fd04af9969dd94c08921
Ricardo <Ricardo@LAPTOP-OGO8FE45>
2018-10-27 23:28:47 -0300

    Exercicio acesso data

--- ExercicioMetodosDeAcessoParaData/test/TestPessoa.java
+++ ExercicioMetodosDeAcessoParaData/test/TestPessoa.java
@@ -10,22 +10,76 @@ import br.coursera.Relogio;
 public class TestPessoa {
 
 	@Test
-	public void deveRetornarIdadePessoaESigno() {
+	public void deveRetornarIdadePessoaESignoCapricornio() {
+
+		LocalDate dataNascimentoRita = LocalDate.of(1980, 01, 20);
+
+		 Relogio relogio = new Relogio(1540692460660L);
+		 
+		//System.out.println(relogio.agora());
 		
-		LocalDate dataNascimentoRita = LocalDate.of(1951, 8, 20); 
-		
-		Relogio relogio = new Relogio(0);
-		
-		System.out.println(relogio.agora());
-		
-		
-		
+        
+
 		Pessoa rita = new Pessoa(dataNascimentoRita);
-		
-		System.out.println(rita.mes());
-		
-		assertEquals(67,rita.getIdade());
-		assertEquals("Leao",rita.getSigno());
+
+		assertEquals(38, rita.getIdade());
+		assertEquals("Capricornio", rita.getSigno());
+
+	}
+	
+	@Test
+	public void deveRetornarIdadePessoaESignoAquario() {
+
+		LocalDate dataNascimentoRita = LocalDate.of(1980, 02, 19);
+
+		// Relogio relogio = new Relogio(1);
+
+		Pessoa rita = new Pessoa(dataNascimentoRita);
+
+		assertEquals(38, rita.getIdade());
+		assertEquals("Aquario", rita.getSigno());
+
+	}
+	
+	@Test
+	public void deveRetornarIdadePessoaESignoAries() {
+
+		LocalDate dataNascimentoRita = LocalDate.of(1980, 03, 21);
+
+		// Relogio relogio = new Relogio(1);
+
+		Pessoa rita = new Pessoa(dataNascimentoRita);
+
+		assertEquals(38, rita.getIdade());
+		assertEquals("Aries", rita.getSigno());
+
+	}
+	
+	@Test
+	public void deveRetornarIdadePessoaESignoTouro() {
+
+		LocalDate dataNascimentoRita = LocalDate.of(1980, 04, 21);
+
+		// Relogio relogio = new Relogio(1);
+
+		Pessoa rita = new Pessoa(dataNascimentoRita);
+
+		assertEquals(38, rita.getIdade());
+		assertEquals("Touro", rita.getSigno());
+
+	}
+	
+	@Test
+	public void deveRetornarIdadePessoaESignoGemeos() {
+
+		LocalDate dataNascimentoRita = LocalDate.of(1980, 05, 21);
+
+		// Relogio relogio = new Relogio(1);
+
+		Pessoa rita = new Pessoa(dataNascimentoRita);
+
+		assertEquals(38, rita.getIdade());
+		assertEquals("Gemeos", rita.getSigno());
 
 	}
 
