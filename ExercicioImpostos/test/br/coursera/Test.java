0e58fb32344e55981840bfaf87381c2d6c51200d
aluno <aluno@10.32.16.106>
2018-11-23 10:02:22 -0200

    Exercicio impostos

--- ExercicioImpostos/test/br/coursera/Test.java
+++ ExercicioImpostos/test/br/coursera/Test.java
@@ -14,10 +14,10 @@ public class Test {
 		listaConstribuintes.add(new PessoaFisica("Joao Santos",3000.00,"11111"));
 		listaConstribuintes.add(new PessoaJuridica("Lojas AA",150000.00,"10055"));
 		listaConstribuintes.add(new PessoaFisica("Maria Soares",5000.00,"22222"));
-		listaConstribuintes.add(new PessoaJuridica("Supermercados B",2000000.00,"10066");
-		listaConstribuintes.add(new PessoaFisica("Carla Maia",1500.00,"33333");
-		listaConstribuintes.add(new PessoaJuridica("Posto XX",500000.00,"10077");
-		listaConstribuintes.add(new PessoaFisica("José da Silva",95000.00,"97687");
+		listaConstribuintes.add(new PessoaJuridica("Supermercados B",2000000.00,"10066"));
+		listaConstribuintes.add(new PessoaFisica("Carla Maia",1500.00,"33333"));
+		listaConstribuintes.add(new PessoaJuridica("Posto XX",500000.00,"10077"));
+		listaConstribuintes.add(new PessoaFisica("José da Silva",95000.00,"97687"));
 		
 		
 		for (Contribuinte contribuinte : listaConstribuintes) {
